5c0ef70bf8aa6c5d8f1fe81d87cd5a219fd27c7d
onmail12 <protonu1122@onmail.com>
2022-10-30 18:49:41 +0800

    tes2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('header')
 
-<h1 class="display-5 fw-bolder text-white mb-2">Website Pembayaran SPP</h1>
+<h1 class="display-5 fw-bolder text-white mb-2">Website Pemsbayaran SPP</h1>
 <p class="lead fw-normal text-white-50 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet impedit delectus deleniti quae cupiditate, qui eum labore eveniet ut ullam veritatis rem fuga assumenda totam architecto ducimus repellendus, corporis recusandae.</p>
 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
     <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Transaksi Pembayaran</a>
